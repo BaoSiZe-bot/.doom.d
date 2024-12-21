09e0b9eb0a017335a54a2e93a32055a52b9d5324
Size Bao <baosize@hotmail.com>
2024-12-21 10:03:45 +0800

    tty-child-frames 并入主线，删除 acm-terminal.

--- modules/baosize/lsp-bridge/config.el
+++ modules/baosize/lsp-bridge/config.el
@@ -10,8 +10,8 @@
   (lsp-bridge-enable-hover-diagnostic t)
   (lsp-bridge-signature-show-function 'lsp-bridge-signature-show-with-frame)
   (lsp-bridge-signature-show-with-frame-position 'point)
-  (acm-enable-capf t)
   (acm-enable-preview t)
+  (acm-enable-yas t)
   :bind (("C-c c r" . lsp-bridge-rename)
          ("C-c c a" . lsp-bridge-code-action)
          ("C-c c j" . lsp-bridge-find-def)
@@ -19,9 +19,4 @@
          ("C-c c x" . lsp-bridge-diagnostic-list)
          ("C-c c n" . lsp-bridge-diagnostic-jump-next)
          ("C-c c p" . lsp-bridge-diagnostic-jump-prev)
-         ("C-c k" . lsp-bridge-popup-documentation))
-  ;;:init
-  ;;(unless (display-graphic-p)
-  ;;(with-eval-after-load 'acm
-  ;;  (require 'acm-terminal)))
-)
+         ("C-c k" . lsp-bridge-popup-documentation)))
