88c3e8cc54c647fa55360831676e2f11c5b460d8
Size Bao <baosize@hotmail.com>
2025-01-08 10:51:22 +0800

    删除blink-search冗余的快捷键。

--- modules/baosize/blink-search/config.el
+++ modules/baosize/blink-search/config.el
@@ -1,6 +1,6 @@
 ;;; $DOOMDIR/modules/baosize/blink-search/config.el -*- lexical-binding: t; -*-
 (add-to-list 'load-path "~/.emacs.d/.local/straight/repos/blink-search/")
-(bind-key "M-RET" 'blink-search)
+(bind-key "C-M-g" 'blink-search)
 (after! blink-search
   (add-hook! 'blink-search-mode-hook (meow-insert))
   (setq blink-search-enable-posframe t))
