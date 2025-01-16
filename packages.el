975eafb046ebd4c2a20718e320882a327457ad94
Size Bao <baosize@hotmail.com>
2025-01-16 13:04:57 +0800

    添加一些功能。

--- packages.el
+++
@@ -1,9 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOMDIR/packages.el
-(unpin! t)
-;;(package! vc-msg)
-;;(package! power-mode)
-;;(package! imenu-list)
-(package! noflet)
-(package! cal-china-x)
-;;(package! railgun :recipe(:host github :repo "gynamics/railgun.el"))
