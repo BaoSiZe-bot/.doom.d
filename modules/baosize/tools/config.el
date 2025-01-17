91348e085f65498006403eeb99265eef15923648
Size Bao <baosize@hotmail.com>
2025-01-17 17:17:58 +0800

    净化 tools 模块，只剩下内置包。

--- modules/baosize/tools/config.el
+++ modules/baosize/tools/config.el
@@ -1,10 +1,4 @@
 ;;; baosize/tools/config.el -*- lexical-binding: t; -*-
-;; Automatically save file content
-
-(use-package keyfreq
-  :defer t
-  :hook (doom-first-input . keyfreq-mode)
-        (doom-first-input . keyfreq-autosave-mode))
 (use-package paren
   :defer t
   :custom-face (show-paren-match ((t (:foreground "SpringGreen3" :underline t :weight bold))))
@@ -12,15 +6,37 @@
   (setq show-paren-when-point-inside-paren t
         show-paren-when-point-in-periphery t
         show-paren-context-when-offscreen t))
-(use-package colorful-mode
-  :defer t
-  :hook (prog-mode text-mode))
-
-    ;; (add-hook 'prog-mode-hook #'symbol-overlay-mode)
-    ;; (add-hook 'text-mode-hook #'symbol-overlay-mode)
-
-(if (modulep! :tools lsp)
-  (add-hook 'lsp-mode-hook #'lsp-headerline-breadcrumb-mode))
+(when (modulep! :tools lsp)
+  (add-hook 'lsp-mode-hook #'lsp-headerline-breadcrumb-mode)
+(defun lsp-booster--advice-json-parse (old-fn &rest args)
+  "Try to parse bytecode instead of json."
+  (or
+   (when (equal (following-char) ?#)
+     (let ((bytecode (read (current-buffer))))
+       (when (byte-code-function-p bytecode)
+         (funcall bytecode))))
+   (apply old-fn args)))
+(advice-add (if (progn (require 'json)
+                       (fboundp 'json-parse-buffer))
+                'json-parse-buffer
+              'json-read)
+            :around
+            #'lsp-booster--advice-json-parse)
+(defun lsp-booster--advice-final-command (old-fn cmd &optional test?)
+  "Prepend emacs-lsp-booster command to lsp CMD."
+  (let ((orig-result (funcall old-fn cmd test?)))
+    (if (and (not test?)                             ;; for check lsp-server-present?
+             (not (file-remote-p default-directory)) ;; see lsp-resolve-final-command, it would add extra shell wrapper
+             lsp-use-plists
+             (not (functionp 'json-rpc-connection))  ;; native json-rpc
+             (executable-find "emacs-lsp-booster"))
+        (progn
+          (when-let ((command-from-exec-path (executable-find (car orig-result))))  ;; resolve command from exec-path (in case not found in $PATH)
+            (setcar orig-result command-from-exec-path))
+          (message "Using emacs-lsp-booster for %s!" orig-result)
+          (cons "emacs-lsp-booster" orig-result))
+      orig-result))))
+(advice-add 'lsp-resolve-final-command :around #'lsp-booster--advice-final-command)
 
 (display-time-mode)
 (fset 'yes-or-no-p 'y-or-n-p)
--- modules/baosize/tools/packages.el
+++
@@ -1,6 +0,0 @@
-;; -*- no-byte-compile: t; -*-
-;;; $DOOOMDIR/modules/baosize/tools/packages.el
-(package! keyfreq
-  :recipe (:host github :repo "dacap/keyfreq"))
-;;(package! prism :recipe (:host github :repo "alphapapa/prism.el"))
-(package! colorful-mode)
