f7df85aeeb46a248aecc0192225258ea34c43440
Size Bao <baosize@hotmail.com>
2025-01-17 14:44:49 +0800

    完全适配 lsp-mode （感觉也不慢啊）

--- modules/baosize/tools/config.el
+++ modules/baosize/tools/config.el
@@ -1,16 +1,6 @@
 ;;; baosize/tools/config.el -*- lexical-binding: t; -*-
 ;; Automatically save file content
-(use-package super-save
-  :diminish
-  :defer 0.5
-  :config
-  (add-to-list 'super-save-triggers 'switch-window)
-  (setq super-save-exclude '(".gpg"))
-  (setq super-save-idle-duration 1)
-  (setq super-save-all-buffers 1)
-  (setq super-save-auto-save-when-idle t)
-  (setq super-save-silent t)
-  (super-save-mode 1))
+
 (use-package keyfreq
   :defer t
   :hook (doom-first-input . keyfreq-mode)
@@ -25,36 +15,12 @@
 (use-package colorful-mode
   :defer t
   :hook (prog-mode text-mode))
-(use-package symbol-overlay
-    :bind
-    (("M-i" . symbol-overlay-put)
-    ("M-n" . symbol-overlay-switch-forward)
-    ("M-p" . symbol-overlay-switch-backward)
-    ("<f7>" . symbol-overlay-mode)
-    ("<f8>" . symbol-overlay-remove-all)))
+
     ;; (add-hook 'prog-mode-hook #'symbol-overlay-mode)
     ;; (add-hook 'text-mode-hook #'symbol-overlay-mode)
 
 (if (modulep! :tools lsp)
   (add-hook 'lsp-mode-hook #'lsp-headerline-breadcrumb-mode))
-
-(use-package winum
-  :defer 0.5
-  :config
-  (winum-mode)
-  :init
-  (setq winum-keymap
-      (let ((map (make-sparse-keymap)))
-        (define-key map (kbd "M-0") 'winum-select-window-0-or-10)
-        (define-key map (kbd "M-1") 'winum-select-window-1)
-        (define-key map (kbd "M-2") 'winum-select-window-2)
-        (define-key map (kbd "M-3") 'winum-select-window-3)
-        (define-key map (kbd "M-4") 'winum-select-window-4)
-        (define-key map (kbd "M-5") 'winum-select-window-5)
-        (define-key map (kbd "M-6") 'winum-select-window-6)
-        (define-key map (kbd "M-7") 'winum-select-window-7)
-        (define-key map (kbd "M-8") 'winum-select-window-8)
-        map)))
 
 (display-time-mode)
 (fset 'yes-or-no-p 'y-or-n-p)
--- modules/baosize/tools/packages.el
+++ modules/baosize/tools/packages.el
@@ -2,10 +2,5 @@
 ;;; $DOOOMDIR/modules/baosize/tools/packages.el
 (package! keyfreq
   :recipe (:host github :repo "dacap/keyfreq"))
-(package! super-save)
 ;;(package! prism :recipe (:host github :repo "alphapapa/prism.el"))
 (package! colorful-mode)
-(package! symbol-overlay)
-(package! breadcrumb)
-(package! winum)
-(package! el-search)
